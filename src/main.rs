1e7a0b6140eef7e632f03b9974f4e6bd786b7438
knipferrc <knipferrc@gmail.com>
2018-11-24 08:41:07 -0500

    working through tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::io::{stdin, stdout, Write};
+use std::path::Path;
 use std::process::Command;
 
 fn main() {
@@ -16,12 +17,23 @@ fn main() {
         let command = parts.next().unwrap();
         let args = parts;
 
-        let mut child = if cfg!(target_os = "windows") {
-            Command::new(command).args(args).spawn().unwrap()
-        } else {
-            Command::new(command).args(args).spawn().unwrap()
-        };
+        match command {
+            "cd" => {
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            },
+            "exit" => return,
+            command => {
+                let mut child = Command::new(command)
+                    .args(args)
+                    .spawn()
+                    .unwrap();
 
-        child.wait().expect("Failed executing command");
+                child.wait().expect("Error spawning command");
+            }
+        }
     }
 }
