5be7d949436d7114f225d8553470ed0ccab8d433
knipferrc <knipferrc@gmail.com>
2019-08-07 18:04:45 -0400

    WIP prompt

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,67 @@
 use std::env;
+use std::str::{SplitWhitespace};
 use std::io::{stdin, stdout, Write};
 use std::path::Path;
-use std::process::{Child, Command, Stdio};
+use std::process::{Command};
 
-fn main() {
+mod colors;
+
+fn change_dir(args: SplitWhitespace) {
+    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+    let root = Path::new(new_dir);
+    if let Err(e) = env::set_current_dir(&root) {
+        eprintln!("{}", e);
+    }
+}
+
+fn generate_prompt() -> String {
+    let path = env::current_dir().unwrap();
+    let split_path: Vec<&str> = path.to_str().unwrap().split("/").collect();
+    
+    let prompt = format!(
+        "{}{}{}{}{}{}{} ", 
+        colors::ANSI_BOLD,
+        colors::GREEN,
+        "~/",
+        split_path[split_path.len() - 2],
+        "/",
+        split_path[split_path.len() - 1],
+        colors::RESET
+    );
+
+    return prompt;
+}
+
+fn run() {
     loop {
-        // use the `>` character as the prompt
-        // need to explicitly flush this to ensure it prints before read_line
-        print!("> ");
-        stdout().flush().unwrap();
+        let prompt = generate_prompt();
+        print!("{}", prompt);
+        stdout().flush().ok().expect("Could not flush stdout");
 
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
+        
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
 
-        // read_line leaves a trailing newline, which trim removes
-        // this needs to be peekable so we can determine when we are on the last command
-        let mut commands = input.trim().split(" | ").peekable();
-        let mut previous_command = None;
+        match command {
+            "cd" => change_dir(args),
+            "exit" => return,
+            command => {
+                let child = Command::new(command)
+                    .args(args)
+                    .spawn();
 
-        while let Some(command) = commands.next() {
-            // everything after the first whitespace character is interpreted as args to the command
-            let mut parts = command.trim().split_whitespace();
-            let command = parts.next().unwrap();
-            let args = parts;
-
-            match command {
-                "cd" => {
-                    // default to '/' as new directory if one was not provided
-                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
-                    let root = Path::new(new_dir);
-                    if let Err(e) = env::set_current_dir(&root) {
-                        eprintln!("{}", e);
-                    }
-
-                    previous_command = None;
-                }
-                "exit" => return,
-                command => {
-                    let stdin = previous_command.map_or(Stdio::inherit(), |output: Child| {
-                        Stdio::from(output.stdout.unwrap())
-                    });
-
-                    let stdout = if commands.peek().is_some() {
-                        // there is another command piped behind this one
-                        // prepare to send output to the next command
-                        Stdio::piped()
-                    } else {
-                        // there are no more commands piped behind this one
-                        // send output to shell stdout
-                        Stdio::inherit()
-                    };
-
-                    let output = Command::new(command)
-                        .args(args)
-                        .stdin(stdin)
-                        .stdout(stdout)
-                        .spawn();
-
-                    match output {
-                        Ok(output) => {
-                            previous_command = Some(output);
-                        }
-                        Err(e) => {
-                            previous_command = None;
-                            eprintln!("{}", e);
-                        }
-                    };
-                }
+                match child {
+                    Ok(mut child) => { child.wait().expect("Error"); },
+                    Err(e) => eprintln!("{}", e),
+                };
             }
         }
-
-        if let Some(mut final_command) = previous_command {
-            // block until the final command has finished
-            final_command.wait().unwrap();
-        }
-
     }
+}
+
+fn main() {
+    run();
 }
