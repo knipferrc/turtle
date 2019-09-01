84421db54fad1b128e1368ff95bdf700dac3dd18
knipferrc <knipferrc@gmail.com>
2019-09-01 08:14:54 -0400

    add support for pipes

--- src/shell.rs
+++ src/shell.rs
@@ -1,5 +1,5 @@
 use std::io::stdin;
-use std::process::Command;
+use std::process::{Child, Command, Stdio};
 
 use crate::commands;
 use crate::prompt;
@@ -11,23 +11,52 @@ pub fn run() {
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
-        let mut parts = input.trim().split_whitespace();
-        let command = parts.next().unwrap();
-        let args = parts;
+        let mut commands = input.trim().split(" | ").peekable();
+        let mut previous_command = None;
 
-        match command {
-            "cd" => commands::change_dir(args),
-            "exit" => return,
-            command => {
-                let child = Command::new(command).args(args).spawn();
+        while let Some(command) = commands.next() {
+            let mut parts = command.trim().split_whitespace();
+            let command = parts.next().unwrap();
+            let args = parts;
 
-                match child {
-                    Ok(mut child) => {
-                        child.wait().expect("Error");
-                    }
-                    Err(e) => eprintln!("{}", e),
-                };
+            match command {
+                "cd" => {
+                    commands::change_dir(args);
+                    previous_command = None;
+                }
+                "exit" => return,
+                command => {
+                    let stdin = previous_command.map_or(Stdio::inherit(), |output: Child| {
+                        Stdio::from(output.stdout.unwrap())
+                    });
+
+                    let stdout = if commands.peek().is_some() {
+                        Stdio::piped()
+                    } else {
+                        Stdio::inherit()
+                    };
+
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+
+                    match output {
+                        Ok(output) => {
+                            previous_command = Some(output);
+                        }
+                        Err(e) => {
+                            previous_command = None;
+                            eprintln!("{}", e);
+                        }
+                    };
+                }
             }
+        }
+
+        if let Some(mut final_command) = previous_command {
+            final_command.wait().unwrap();
         }
     }
 }
